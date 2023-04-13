747c1b881a12fb6cb96f002475aae557b3ea9290
Neolize <rumaria.work@gmail.com>
2023-04-13 17:27:59 +0300

    Created and connected search.js

---
+++ static/rating_movies/js/search.js
@@ -0,0 +1,15 @@
+"use strict";
+
+
+function printMessage() {
+    let message = "Some message";
+    console.log(message);
+}
+
+
+function main() {
+    printMessage();
+}
+
+
+document.addEventListener("DOMContentLoaded", main);
