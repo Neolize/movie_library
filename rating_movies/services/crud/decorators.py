e79a697200c72936d22f17d0eb9c4db926c6bff7
Neolize <rumaria.work@gmail.com>
2023-04-15 11:10:44 +0300

    Added ordering decorator for 'Actor' model queries

--- rating_movies/services/crud/decorators.py
+++ rating_movies/services/crud/decorators.py
@@ -8,7 +8,7 @@ LOGGER = logging.getLogger("json_main_logger")
 
 
 def base_movie_filter(func: callable) -> callable:
-    """Set base filter for model "Movie" queries"""
+    """Set base filter for "Movie" model queries"""
     def base_movie_filter_wrapper(*args, **kwargs) -> Optional[QuerySet]:
         try:
             query_set = func(*args, **kwargs).filter(draft=False)
@@ -21,7 +21,7 @@ def base_movie_filter(func: callable) -> callable:
 
 
 def base_movie_ordering(func: callable) -> callable:
-    """Set base ordering for model "Movie" queries"""
+    """Set base ordering for "Movie" model queries"""
     def base_movie_ordering_wrapper(*args, **kwargs) -> Optional[QuerySet]:
         try:
             query_set = func(*args, **kwargs).order_by("-world_premiere")
@@ -31,3 +31,16 @@ def base_movie_ordering(func: callable) -> callable:
 
         return query_set
     return base_movie_ordering_wrapper
+
+
+def base_actor_director_ordering(func: callable) -> callable:
+    """Set base ordering for "Actor" model queries"""
+    def base_actor_director_ordering_wrapper(*args, **kwargs) -> Optional[QuerySet]:
+        try:
+            query_set = func(*args, **kwargs).order_by("name")
+        except AttributeError as attr_exc:
+            LOGGER.error(attr_exc)
+            query_set = None
+
+        return query_set
+    return base_actor_director_ordering_wrapper
